70b5f6a97d831d2eb6807ef2e0b58b2769e53aeb
Saurabh <148630179+saurabh3126@users.noreply.github.com>
2025-10-28 00:55:27 +0530

    Time: 9 ms (8.39%), Space: 10.8 MB (15.26%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // 1. Convert the number to a string
-        // We can now use "string" and "to_string" directly
+        
         string s = to_string(x);
         
-        // 2. Create a reversed copy
-        string reversed_s = s;
-        // We can also use "reverse" directly
-        reverse(reversed_s.begin(), reversed_s.end());
-        
-        // 3. Compare them
-        return s == reversed_s;
+        string rev = s;
+
+        reverse(rev.begin(), rev.end());
+
+        return s == rev;
     }
 };

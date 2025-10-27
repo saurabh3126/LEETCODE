692657d90d1abdfe3b6fb590a5f03c02297c7168
Saurabh <148630179+saurabh3126@users.noreply.github.com>
2025-10-27 23:47:47 +0530

    Time: 11 ms (31.87%), Space: 27.2 MB (84.17%) - LeetHub

---
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int n = bank.size();
+        int prev = 0;
+        int result = 0;
+        for(int i = 0 ; i < n ; i++){
+          int curr = 0 ;
+          for( char &ch : bank[i] ){
+             if(ch == '1'){
+                curr++;
+             }
+          }
+                result += (prev * curr) ;
+                if ( curr != 0){
+                    prev = curr;
+                }
+
+           
+         
+  
+
+          }
+          
+
+            return result;
+       
+
+    }
+
+    
+
+};

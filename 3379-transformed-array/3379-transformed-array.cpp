13a1204eeff17791f43164c52636be8ff3f63c81
Saurabh <148630179+saurabh3126@users.noreply.github.com>
2026-02-05 22:56:30 +0530

    Time: 0 ms (100%), Space: 25.8 MB (39.55%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n =nums.size();
+        vector<int> result(n);
+
+        for(int i =0 ; i<n ; i++){
+            int shift =nums[i]%n;
+            int newIndex = (i+shift)%n;
+            if(newIndex < 0){
+                newIndex += n;
+            }
+            result[i]=nums[newIndex];
+        }
+               
+      return result;
+    }    
+    
+};

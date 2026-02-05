a9105e852c9c94c9e6f6696ba9d0bf7e85cf68d6
Saurabh <148630179+saurabh3126@users.noreply.github.com>
2026-02-05 23:09:59 +0530

    Time: 8 ms (46.52%), Space: 25.8 MB (39.55%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         int n =nums.size();
-        vector<int> result(n,0);
+        vector<int> result(n);
 
         for(int i =0 ; i<n ; i++){
             int shift =nums[i]%n;
